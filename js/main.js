b091a9a52b7c309df3730d3c2b1b1b21e253232f
Piotr Pawlik <59952821+kilwap@users.noreply.github.com>
2021-11-30 18:57:36 +0100

    added show/hide project description

--- js/main.js
+++ js/main.js
@@ -2,24 +2,33 @@ let project1 = document.getElementById("project1");
 let project2 = document.getElementById("project2");
 let project3 = document.getElementById("project3");
 let description1 = document.getElementById("description1");
+let description2 = document.getElementById("description2");
+let description3 = document.getElementById("description3");
 
-//function showmore1(){
-    
-    //project1.style.color = "pink";
+function showmore1(){
+    if (description1.style.display == "none"){
+      description1.style.display = "block";
+   }else {
+      description1.style.display = "none";
+   }  
+}
 
-   // if description1.style.display == "none"{
-   //     description1.style.display = "block";
-   // }else {
-  //      description1.style.display = "none";
-  //  }
-    
-//}
+function showmore2(){
+  if (description2.style.display == "none"){
+    description2.style.display = "block";
+ }else {
+    description2.style.display = "none";
+ }  
+}
 
-//project1.addEventListener("click", showmore1 );
+function showmore3(){
+  if (description3.style.display == "none"){
+    description3.style.display = "block";
+ }else {
+    description3.style.display = "none";
+ }  
+}
 
-
-function showInfo(){
-    description1.style.display = 'block';
-  }
-  
-  project1.addEventListener('click', showInfo)
+project1.addEventListener("click", showmore1);
+project2.addEventListener("click", showmore2);
+project3.addEventListener("click", showmore3);
